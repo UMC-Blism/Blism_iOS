6540a9d3421c024e779db7de22799a8e3c051c9a
Azin <setiguy@gachon.ac.kr>
2024-12-19 21:20:01 +0900

    [refactor/#48]: navigationBar icon 변경

--- Blism/Controller/SelectDoorDesignViewController.swift
+++ Blism/Controller/SelectDoorDesignViewController.swift
@@ -15,10 +15,26 @@ class SelectDoorDesignViewController: UIViewController {
         super.viewDidLoad()
 
         view = selectDoorView
-        self.navigationItem.titleView = NavigationTitleView(title: "디자인 선택", titleColor: .blismBlack)
+
         
         setAction()
         setupDelegate()
+        setNavigationBar()
+    }
+    
+    private func setNavigationBar(){
+        // 뒤로 가기 버튼
+        let leftBarButton = UIBarButtonItem(image: .popIcon, style: .plain, target: self, action: #selector(popAction))
+        leftBarButton.tintColor = .blismBlack
+        self.navigationItem.setLeftBarButton(leftBarButton, animated: true)
+        
+        // 타이틀
+        self.navigationItem.titleView = NavigationTitleView(title: "디자인 선택", titleColor: .blismBlack)
+    }
+    
+    @objc
+    private func popAction(){
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func setAction() {
